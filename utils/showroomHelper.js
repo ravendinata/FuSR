41743b15eadccef4d19a23402ce75280ac274593
ravendinata <61657795+ravendinata@users.noreply.github.com>
2022-01-23 14:54:43 +0700

    Update showroomHelper.js
    - Updated API fetch method to use API getter function
    - Fixed empty timetable endless loop error

--- utils/showroomHelper.js
+++ utils/showroomHelper.js
@@ -51,6 +51,12 @@ function getGiftInfo(search_param)
 
 function getGiftPoint(gift_id) { try { return getGiftInfo(gift_id)[0].point } catch(ex) {}; }
 
+async function getAPI(url)
+{
+    const response = await fetch(url, METHOD_GET);
+    return await response.json();
+}
+
 /*
     EXPORTED
 */
@@ -60,56 +66,53 @@ async function getOnlive(param)
     const status = new Spinner(" > Fetching data...");
     status.start();
 
-    fetch(BASE_ONLIVE_API_URL, METHOD_GET)
-    .then(res => res.json())
-    .then((json) =>
+    var result = await getAPI(BASE_ONLIVE_API_URL);
+    var data = result.onlives[0].lives;
+    
+    var r1 = data.filter(function(x)
+    { return x.room_url_key.startsWith("akb48_"); })
+    var r2 = data.filter(function(x)
+    { return x.room_url_key.startsWith("48_"); })
+
+    var result = r1.concat(r2);
+
+    if (param != undefined)
     {
-        var data = json.onlives[0].lives;
-        var r1 = data.filter(function(x)
-        { return x.room_url_key.startsWith("akb48_"); })
-        var r2 = data.filter(function(x)
-        { return x.room_url_key.startsWith("48_"); })
+        var q1 = result.filter(function(x)
+        { return x.room_url_key.toLowerCase().includes(param); })
 
-        var result = r1.concat(r2);
+        var q2 = result.filter(function(x)
+        { return x.main_name.toLowerCase().includes(param); })
 
-        if (param != undefined)
-        {
-            var q1 = result.filter(function(x)
-            { return x.room_url_key.toLowerCase().includes(param); })
+        result = q1.concat(q2);
+        var set = new Set(result);
+        result = Array.from(set);
+    }
 
-            var q2 = result.filter(function(x)
-            { return x.main_name.toLowerCase().includes(param); })
+    const roomCount = result.length;
 
-            result = q1.concat(q2);
-            var set = new Set(result);
-            result = Array.from(set);
-        }
+    if (roomCount == 0)
+    {
+        console.info(chalk.bgHex('#e05600')("No members are live streaming.\n"));
+        return;
+    }
 
-        status.stop();
-        const roomCount = result.length;
-
-        console.info("\n=== Streaming Now ===\n");
-
-        if (roomCount == 0)
-        {
-            console.info(chalk.bgHex('#e05600')("No members are live streaming.\n"));
-            return;
-        }
-
-        const rows = [];
-        for (let room = 0; room < roomCount; room++)
-        {
-            rows.push
-            ({
-                'Room Name': result[room].main_name,
-                'Room ID': result[room].room_id,
-                'URL': BASE_URL + '/' + result[room].room_url_key
-            });
-        }
-        console.table(rows);
-
-        console.info(`\n> ${roomCount} Members Streaming | Success!\n`);
-    })
+    const rows = [];
+    for (let room = 0; room < roomCount; room++)
+    {
+        rows.push
+        ({
+            'Room Name': result[room].main_name,
+            'Room ID': result[room].room_id,
+            'URL': BASE_URL + '/' + result[room].room_url_key
+        });
+    }
+    
+    status.stop();
+    
+    console.info("\n=== Streaming Now ===\n");
+    console.table(rows);
+    console.info(`\n> ${roomCount} Members Streaming | Success!\n`);
 }
 
 async function getRoomInfo(room_id)
@@ -121,45 +124,43 @@ async function getRoomInfo(room_id)
     const status = new Spinner(" > Fetching data...");
     status.start();
 
-    fetch(BASE_API_URL + endpoint, METHOD_GET)
-    .then(res => res.json())
-    .then(json => 
+    var json = await getAPI(BASE_API_URL + endpoint);
+
+
+    var isLive;
+    var currStreamStart = convertEpochTo24hr(json.current_live_started_at);
+
+    console.log(`> Fetch Check: ${json.main_name}`);
+
+    if (json.main_name == undefined)
     {
-        status.stop();
+        console.info(chalk.bgRed(`\nCannot find room with id = ${room_id}! Please check ID...\n`));
+        return;
+    }
 
-        var isLive;
-        var currStreamStart = convertEpochTo24hr(json.current_live_started_at);
+    if (json.is_onlive == true)
+        isLive = "Yes [Online]";
+    else
+    {
+        isLive = "Not Streaming [Offline]";
+        currStreamStart = isLive;
+    }
 
-        console.log(`> Fetch Check: ${json.main_name}`);
+    var rows = [];
+    rows.push({'Property': 'Name', 'Value': json.main_name});
+    rows.push({'Property': 'Room Level', 'Value': json.room_level});
+    rows.push({'Property': 'Room ID', 'Value': json.room_id});
+    rows.push({'Property': 'Follower', 'Value': json.follower_num});
+    rows.push({'Property': 'Currently Streaming?', 'Value': isLive});
+    rows.push({'Property': 'Current Stream Start Time', 'Value': currStreamStart});
+    rows.push({'Property': 'Current Viewer', 'Value': json.view_num});
 
-        if (json.main_name == undefined)
-        {
-            console.info(chalk.bgRed(`\nCannot find room with id = ${room_id}! Please check ID...\n`));
-            return;
-        }
+    status.stop();
 
-        if (json.is_onlive == true)
-            isLive = "Yes [Online]";
-        else
-        {
-            isLive = "Not Streaming [Offline]";
-            currStreamStart = isLive;
-        }
-
-        var rows = [];
-        rows.push({'Property': 'Name', 'Value': json.main_name});
-        rows.push({'Property': 'Room Level', 'Value': json.room_level});
-        rows.push({'Property': 'Room ID', 'Value': json.room_id});
-        rows.push({'Property': 'Follower', 'Value': json.follower_num});
-        rows.push({'Property': 'Currently Streaming?', 'Value': isLive});
-        rows.push({'Property': 'Current Stream Start Time', 'Value': currStreamStart});
-        rows.push({'Property': 'Current Viewer', 'Value': json.view_num});
-
-        console.table(rows);
-        console.info('\n=== Room Description ===\n');
-        console.info(json.description);
-        console.info('\n');
-    })
+    console.table(rows);
+    console.info('\n=== Room Description ===\n');
+    console.info(json.description);
+    console.info('\n');
 }
 
 // Stage Related
@@ -173,26 +174,21 @@ async function getStageUserList(room_id, n = 13)
     const status = new Spinner(" > Fetching data...");
     status.start();
 
-    fetch(BASE_API_URL + endpoint, METHOD_GET)
-    .then(res => res.json())
-    .then(json => 
+    var result = await getAPI(BASE_API_URL + endpoint);
+    var data = result.stage_user_list;
+
+    if (data[0] == null)
     {
-        status.stop();
+        console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
+        return;
+    }
 
-        const data = json.stage_user_list;
+    var rows = [];
+    for (let i = 0; i < n && i < 100; i++)
+        rows.push({'Username': data[i].user.name, 'UID': data[i].user.user_id});
 
-        if (data[0] == null)
-        {
-            console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
-            return;
-        }
-
-        var rows = [];
-        for (let i = 0; i < n && i < 100; i++)
-            rows.push({'Username': data[i].user.name, 'UID': data[i].user.user_id});
-
-        console.table(rows);
-    })
+    status.stop();
+    console.table(rows);
 }
 
 async function searchStage(room_id, search_param)
@@ -210,44 +206,39 @@ async function searchStage(room_id, search_param)
     const status = new Spinner(" > Fetching data...");
     status.start();
 
-    fetch(BASE_API_URL + endpoint, METHOD_GET)
-    .then(res => res.json())
-    .then(json => 
+    var result= await getAPI(BASE_API_URL + endpoint);
+    var data = result.stage_user_list;
+
+    if (data[0] == null)
     {
-        status.stop();
+        console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
+        return;
+    }
 
-        const data = json.stage_user_list;
+    var id_search = data.filter(function(x)
+    { return x.gift_id === Number(search_param); });
 
-        if (data[0] == null)
-        {
-            console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
-            return;
-        }
+    try
+    {
+        var string_search = data.filter(function(x)
+        { return x.user.name.toLowerCase().includes(search_param.toLowerCase()); });
+    }
+    catch(ex) {}
 
-        var id_search = data.filter(function(x)
-        { return x.gift_id === Number(search_param); });
+    var user = id_search.concat(string_search);
+    var rows = [];
+    for (let i = 0; i < user.length; i++)
+    {
+        rows.push
+        ({
+            'Username': user[i].user.name, 
+            'UID': user[i].user.user_id,
+            'Rank': user[i].rank
+        });
+    }
 
-        try
-        {
-            var string_search = data.filter(function(x)
-            { return x.user.name.toLowerCase().includes(search_param.toLowerCase()); });
-        }
-        catch(ex) {}
-
-        var user = id_search.concat(string_search);
-        var rows = [];
-        for (let i = 0; i < user.length; i++)
-        {
-            rows.push
-            ({
-                'Username': user[i].user.name, 
-                'UID': user[i].user.user_id,
-                'Rank': user[i].rank
-            });
-        }
-
-        console.table(rows);
-    })
+    status.stop(); 
+    console.table(rows);
 }
 
 async function getStreamUrl(room_id)
@@ -259,28 +250,23 @@ async function getStreamUrl(room_id)
     const status = new Spinner(" > Fetching data...");
     status.start();
 
-    fetch(BASE_API_URL + endpoint, METHOD_GET)
-    .then(res => res.json())
-    .then(json => 
+    var result = await getAPI(BASE_API_URL + endpoint);
+    var data = result.streaming_url_list;
+
+    if (data == null)
     {
-        status.stop();
-        
-        const data = json.streaming_url_list;
+        console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
+        return;
+    }
 
-        if (data == null)
-        {
-            console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
-            return;
-        }
+    let urlCount = data.length;
 
-        let urlCount = data.length;
+    var rows = [];
+    for (let i = 0; i < urlCount; i++)
+        rows.push({'Quality': data[i].label + ` [${data[i].quality}/${data[i].type}]`, 'URL': data[i].url})
 
-        var rows = [];
-        for (let i = 0; i < urlCount; i++)
-            rows.push({'Quality': data[i].label + ` [${data[i].quality}/${data[i].type}]`, 'URL': data[i].url})
-
-        console.table(rows);
-    })
+    status.stop();
+    console.table(rows);
 }
 
 // Gift Related
@@ -294,46 +280,41 @@ async function getGiftable(room_id, dump)
     const status = new Spinner(" > Fetching data...");
     status.start();
 
-    fetch(BASE_API_URL + endpoint, METHOD_GET)
-    .then(res => res.json())
-    .then(json => 
+    var json = await getAPI(BASE_API_URL + endpoint);    
+    var data = json.normal;
+
+    if (dump == true)
     {
-        status.stop();
-        
-        const data = json.normal;
+        let odata = JSON.stringify(json, null, 4);
+        fs.writeFileSync('gift_list.json', odata);
+        console.info(chalk.bgBlueBright("\nDumping Done!\n"));
+        return;
+    }
 
-        if (dump == true)
-        {
-            let odata = JSON.stringify(json, null, 4);
-            fs.writeFileSync('gift_list.json', odata);
-            console.info(chalk.bgBlueBright("\nDumping Done!\n"));
-            return;
-        }
+    if (data[0] == null)
+    {
+        console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
+        return;
+    }
 
-        if (data[0] == null)
-        {
-            console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
-            return;
-        }
+    let items = data.length;
+    console.info(`\n${items} items giftable in this room\n`);
 
-        let items = data.length;
-        console.info(`\n${items} items giftable in this room\n`);
+    var rows = [];
+    for (let i = 0; i < items; i++)
+    {
+        rows.push
+        ({
+            'Gift Name': data[i].gift_name, 
+            'Gift ID': data[i].gift_id,
+            'Image URL': data[i].image,
+            'Point': data[i].point,
+            'Free?': data[i].free
+        });
+    }
 
-        var rows = [];
-        for (let i = 0; i < items; i++)
-        {
-            rows.push
-            ({
-                'Gift Name': data[i].gift_name, 
-                'Gift ID': data[i].gift_id,
-                'Image URL': data[i].image,
-                'Point': data[i].point,
-                'Free?': data[i].free
-            });
-        }
-
-        console.table(rows);
-    })
+    status.stop();
+    console.table(rows);
 }
 
 async function getGiftLog(room_id)
@@ -344,47 +325,42 @@ async function getGiftLog(room_id)
 
     const status = new Spinner(" > Fetching data...");
     status.start();
+    
+    var json = await getAPI(BASE_API_URL + endpoint);
+    const data = json.gift_log;
 
-    fetch(BASE_API_URL + endpoint, METHOD_GET)
-    .then(res => res.json())
-    .then(json => 
+    if (data[0] == null)
     {
-        status.stop();
-        
-        const data = json.gift_log;
+        console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
+        return;
+    }
 
-        if (data[0] == null)
+    let items = data.length;
+    
+    var rows = [];
+    for (let i = 0; i < items; i++)
+    {
+        var gift_name = '';
+        try
         {
-            console.info(chalk.bgRed(`\nERROR! Either ID is invalid or room is not currently streaming! Please check ID or try again later...\n`));
-            return;
+            gift_name = getGiftInfo(data[i].gift_id)[0].gift_name;
         }
+        catch(ex) {}
 
-        let items = data.length;
-        
-        var rows = [];
-        for (let i = 0; i < items; i++)
-        {
-            var gift_name = '';
-            try
-            {
-               gift_name = getGiftInfo(data[i].gift_id)[0].gift_name;
-            }
-            catch(ex) {}
+        rows.push
+        ({
+            'Gifter Name': data[i].name, 
+            'Gifter ID': data[i].user_id,
+            'Gift Name': gift_name,
+            'Gift ID': data[i].gift_id,
+            'Time Gifted': convertEpochTo24hr(data[i].created_at),
+            'Quantity': data[i].num,
+            'Total Point': data[i].num * Number(getGiftPoint(data[i].gift_id))
+        });
+    }
 
-            rows.push
-            ({
-                'Gifter Name': data[i].name, 
-                'Gifter ID': data[i].user_id,
-                'Gift Name': gift_name,
-                'Gift ID': data[i].gift_id,
-                'Time Gifted': convertEpochTo24hr(data[i].created_at),
-                'Quantity': data[i].num,
-                'Total Point': data[i].num * Number(getGiftPoint(data[i].gift_id))
-            });
-        }
-
-        console.table(rows);
-    })
+    status.stop();
+    console.table(rows);
 }
 
 function searchGift(search_param)
@@ -405,44 +381,42 @@ function searchGift(search_param)
     console.table(rows);
 }
 
-function getTimetable()
+async function getTimetable()
 {
     const status = new Spinner(" > Fetching data...");
     status.start();
 
-    fetch(BASE_TIMETABLE_API_URL, METHOD_GET)
-    .then(res => res.json())
-    .then((json) =>
+    var json = await getAPI(BASE_TIMETABLE_API_URL);
+    var data = json.time_tables;
+    var result = data.filter(function(x)
+    { return x.room_url_key.startsWith("48_"); })
+
+    const scheduledCount = result.length;
+
+    console.info("\n=== Scheduled Stream ===\n");
+
+    if (scheduledCount == 0)
     {
-        var data = json.time_tables;
-        var result = data.filter(function(x)
-        { return x.room_url_key.startsWith("48_"); })
-
+        console.info(chalk.bgHex('#e05600')("No members scheduled a stream.\n"));
         status.stop();
-        const scheduledCount = result.length;
+        return;
+    }
 
-        console.info("\n=== Scheduled Stream ===\n");
+    const rows = [];
+    for (let room = 0; room < scheduledCount; room++)
+    {
+        rows.push
+        ({
+            'Room Name': result[room].main_name,
+            'Room ID': result[room].room_id,
+            'URL': BASE_URL + '/' + result[room].room_url_key
+        });
+    }
+    
+    status.stop();
 
-        if (scheduledCount == 0)
-        {
-            console.info(chalk.bgHex('#e05600')("No members scheduled a stream.\n"));
-            return;
-        }
-
-        const rows = [];
-        for (let room = 0; room < scheduledCount; room++)
-        {
-            rows.push
-            ({
-                'Room Name': result[room].main_name,
-                'Room ID': result[room].room_id,
-                'URL': BASE_URL + '/' + result[room].room_url_key
-            });
-        }
-        console.table(rows);
-
-        console.info(`\n> ${scheduledCount} Members Streaming | Success!\n`);
-    })    
+    console.table(rows);
+    console.info(`\n> ${scheduledCount} Members Streaming | Success!\n`);
 }
 
 
