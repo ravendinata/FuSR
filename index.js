021e0c68fc44c4b774cfabc64c9c24aef231a136
ravendinata <61657795+ravendinata@users.noreply.github.com>
2022-01-24 01:03:47 +0700

    Option selection method update
    - Option menu is now shown and prompted again until user quits the instance
    - Added exit option to leave the instance

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ Streaming Functions:
 14. Gift Log
 
 FuSRpedia:
-20. Search Gift`;
+20. Search Gift
+
+Utilities:
+0. Exit`;
 
 
 /* MAIN */
@@ -33,73 +36,88 @@ console.info
     )
 );
 
-console.info(options);
-console.info("====================");
-var opt = Number(prompt("Choose Function: "));
+(async () => 
+    {
+        var status = "Running";
 
-switch (opt)
-{
-    case 1:
-        var filter = prompt("Enter filter: ");
-        sr.getOnlive(filter);
-        break;
-    
-    case 2: 
-        var room_id = prompt("Room ID: ");
-        sr.getRoomInfo(room_id);
-        break;
+        while(status == "Running")
+        {
+            console.info("====================");
+            console.info(options);
+            console.info("====================");
+            var opt = Number(prompt("Choose Function: "));
 
-    case 3:
-        sr.getTimetable();
-        break;
+            switch (opt)
+            {
+                case 1:
+                    var filter = prompt("Enter filter: ");
+                    await sr.getOnlive(filter);
+                    break;
+                
+                case 2: 
+                    var room_id = prompt("Room ID: ");
+                    await sr.getRoomInfo(room_id);
+                    break;
 
-    case 4:
-        var filter = prompt("Enter filter: ");
-        sr.getScheduled(filter);
-        break;
-    
-    case 10:
-        var room_id = prompt("Room ID: ");
-        var dispNum = prompt("Users to display (0 to n; Default = 13): ");
-        if (dispNum < 1)
-            dispNum = 13;
-        sr.getStageUserList(room_id, dispNum);
-        break;
+                case 3:
+                    sr.getTimetable();
+                    break;
 
-    case 11:
-        var room_id = prompt("Room ID: ");
-        var param = prompt("Search name: ");
-        sr.searchStage(room_id, param);
-        break;
+                case 4:
+                    var filter = prompt("Enter filter: ");
+                    sr.getScheduled(filter);
+                    break;
+                
+                case 10:
+                    var room_id = prompt("Room ID: ");
+                    var dispNum = prompt("Users to display (0 to n; Default = 13): ");
+                    if (dispNum < 1)
+                        dispNum = 13;
+                    sr.getStageUserList(room_id, dispNum);
+                    break;
 
-    case 12:
-        var room_id = prompt("Room ID: ");
-        sr.getStreamUrl(room_id);
-        break;
+                case 11:
+                    var room_id = prompt("Room ID: ");
+                    var param = prompt("Search name: ");
+                    sr.searchStage(room_id, param);
+                    break;
 
-    case 13:
-        var room_id = prompt("Room ID: ");
-        sr.getGiftable(room_id, false);
-        break;
+                case 12:
+                    var room_id = prompt("Room ID: ");
+                    sr.getStreamUrl(room_id);
+                    break;
 
-    case 14:
-        var room_id = prompt("Room ID: ");
-        sr.getGiftLog(room_id);
-        break;
+                case 13:
+                    var room_id = prompt("Room ID: ");
+                    sr.getGiftable(room_id, false);
+                    break;
 
-    case 20:
-        var param = prompt("Gift ID or Name (Empty = All): ");
-        sr.searchGift(param);
-        break;
+                case 14:
+                    var room_id = prompt("Room ID: ");
+                    sr.getGiftLog(room_id);
+                    break;
 
-    // 'Secret' Options
+                case 20:
+                    var param = prompt("Gift ID or Name (Empty = All): ");
+                    sr.searchGift(param);
+                    break;
 
-    case 486:
-        var room_id = prompt("Room ID: ");
-        sr.getGiftable(room_id, true);
-        break;
+                // 'Secret' Options
 
-    default:
-        console.info("\nOption not found! Exitting...\n");
-        break;
-}
+                case 486:
+                    var room_id = prompt("Room ID: ");
+                    sr.getGiftable(room_id, true);
+                    break;
+
+                case 0:
+                    status = "Quit";
+                    console.info("\nExitting...\n");
+                    break;
+
+                default:
+                    console.info("\nOption not found! Exitting...\n");
+                    break;
+            }
+        }
+    }
+) ()
